a70bcbc449fa22bdb68eb1cc88763e88dd21125e
Lucas Gabriel <lg139826065@gmail.com>
2024-09-14 10:24:59 -0300

    Criação dos Repositorys

---
+++ ProjetoModeloDDD.Infra.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,17 @@
+﻿
+
+using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoModeloDDD.Infra.Data.Repositories
+{
+    public class ProdutoRepository : RespositoryBase<Produto>, IProdutoRepository
+    {
+        public IEnumerable<Produto> BuscarPorNome(string nome)
+        {
+            return Db.Produtos.Where(p => p.Nome == nome);
+        }
+    }
+}
