98ae6d7fe35ab524aa395237a92c8bfa3e4942e5
Lucas Gabriel <lg139826065@gmail.com>
2024-09-14 11:21:05 -0300

    Configuração do AutoMapper

---
+++ ProjetoModeloDDD.Mvc/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿
+using AutoMapper;
+using ProjetoModeloDDD.Mvc.ViewModels;
+using ProjetoModeloDDD.Domain.Entities;
+
+namespace ProjetoModeloDDD.Mvc.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "ViewModelToDomainMappings"; }
+        }
+         
+        protected override void Configure()
+        {
+            Mapper.CreateMap<ClienteViewModel,Cliente>();
+            Mapper.CreateMap<ProdutoViewModel,Produto>();
+        }
+    }
+}
---
+++ ProjetoModeloDDD.Mvc/AutoMapper/ViewoModelToDomainMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Mvc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoModeloDDD.Mvc.AutoMapper
+{
+    public class ViewoModelToDomainMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "DomainToViewModelMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<Cliente, ClienteViewModel > ();
+            Mapper.CreateMap<Produto, ProdutoViewModel > ();
+        }
+
+    }
+}
